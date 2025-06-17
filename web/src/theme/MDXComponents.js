d54ef298ef7aec30a65c5dccfbd8cd61236efacd
Pierre-Olivier Brillant <PierreOlivierBrillant@users.noreply.github.com>
2025-06-17 13:49:42 -0400

    Ajout du composant calendar

--- web/src/theme/MDXComponents.js
+++ web/src/theme/MDXComponents.js
@@ -5,6 +5,8 @@ import Row from "@site/src/components/Row";
 import Video from "@site/src/components/Video";
 import GHCode from "@site/src/components/GHCode";
 import MainDocsGrid from "@site/src/components/MainDocsGrid";
+import MainDocsCalendar from "@site/src/components/MainDocsCalendar";
+import DocsViewer from "@site/src/components/DocsViewer";
 
 export default {
   ...MDXComponents,
@@ -14,4 +16,6 @@ export default {
   Video,
   GHCode,
   MainDocsGrid,
+  MainDocsCalendar,
+  DocsViewer,
 };
