ce978275a136eac90c8e7378e38a29f8b3847870
Maxime Pelletier <90477209+MaximePelletier15@users.noreply.github.com>
2025-07-01 16:27:38 -0400

    déploiement

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 const config = {
     nom: "Introduction à la programmation (TIM)",
-    description: "Site de référence du cours Introduction à la programmation (TIM)",
+    description: "Site de référence du cours 905 - Introduction à la programmation (TIM)",
     nomUrl: "905-IntroProg"
 }
 
