2338fc48104d7a07f73194307051f754bfe6acbb
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-09-22 14:24:35 -0400

    Ajout d'une option pour centrer verticalement le contenu d'une colonne.

--- web/src/components/Column/index.tsx
+++ web/src/components/Column/index.tsx
@@ -1,13 +1,22 @@
-import React, { ReactNode } from "react";
+import React, { JSX, ReactNode } from "react";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 export default function Column({
   children,
   size,
+  vCenter = false,
 }: {
   children: ReactNode;
   size?: number;
+  vCenter?: boolean;
 }): JSX.Element {
   const strSize = size != null ? "col-" + size : "col";
-  return <div className={strSize}>{children}</div>;
+  return (
+    <div
+      className={strSize}
+      style={{ display: "flex", alignItems: vCenter ? "center" : "flex-start" }}
+    >
+      {children}
+    </div>
+  );
 }
