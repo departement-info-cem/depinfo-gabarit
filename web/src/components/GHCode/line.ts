cda56762354176b23cab69a6dcefc3fdb8bf38d5
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-07-16 15:32:57 -0400

    Ajout de la configuration allowScripts pour sharp et amélioration de la classe Line

--- web/src/components/GHCode/line.ts
+++ web/src/components/GHCode/line.ts
@@ -2,6 +2,11 @@ export class Line {
   code: string;
   lineNumber: number;
 
+  constructor(code: string, lineNumber: number) {
+    this.code = code;
+    this.lineNumber = lineNumber;
+  }
+
   toString(): string {
     return this.code;
   }
