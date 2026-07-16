b7816e5c8213be2e7519270ab153a6f57a641448
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-07-16 14:27:27 -0400

    Ajout de l'importation de JSX dans le composant NonVoyant

--- web/src/components/NonVoyant/index.tsx
+++ web/src/components/NonVoyant/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { JSX, ReactNode } from "react";
 
 export default function NonVoyant({ children }: { children: ReactNode }): JSX.Element {
   return (
