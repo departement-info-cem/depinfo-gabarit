536e5f411ea0d9f4d26877c3cb648c723490337e
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-09-21 14:09:30 -0400

    Utilisation de la police du CÉGEP

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -26,6 +26,22 @@ const config = {
     locales: ["fr"],
   },
 
+  stylesheets: [
+    {
+      href: 'https://fonts.googleapis.com',
+      rel: 'preconnect',
+    },
+    {
+      href: 'https://fonts.gstatic.com',
+      rel: 'preconnect',
+      crossorigin: 'anonymous',
+    },
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap',
+      rel: 'stylesheet',
+    },
+  ],
+
   markdown: {
     mermaid: true,
     hooks: {
